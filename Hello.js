a44854a3af527efa72dc3118bc07744bf29ea810
SunnyRoyCode <87200621+SunnyRoyCode@users.noreply.github.com>
2021-07-14 09:16:36 +0530

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,6 @@
+function getdata() {
+    var Name = document.getElementById('name');
+    var Value = Name.value;
+    document.getElementById('page').innerText = " Hello !! " + Value + "  <Welcome to this Website>.";
+    alert('Hello there !! Welcome to this Website.');
+}
